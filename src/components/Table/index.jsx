a6111d6bcd65dbd273a4711abb004999a65b2a94
Gilby Koloay <lolwtf10001@gmail.com>
2023-05-11 02:20:51 +0800

    finished title of 'Table' component

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,10 +3,15 @@ import './style.css';
 
 
 
-export default function Table() {
+export default function Table({ title='<table title>', tambahOnClick=() => console.log('<tambah onClick>') }) {
   return (
     <div id='table-component' className='component'>
-      Table
+      <div className='title'>{title}</div>
+      <button className='tambahButton' onClick={tambahOnClick}>Tambah</button>
+
+      <table>
+        
+      </table>
     </div>
   );
 };
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,12 +7,18 @@ import { Dashboard as DashboardComponent, Header, Table } from '../../components
 
 
 export default function Dashboard() {
+  function tambahOnClick() {
+    console.log('hello world');
+  }
+
+
+
   return (
     <div id='dashboard-page' className='page pageDashboard'>
       <DashboardComponent />
       <main>
         <Header />
-        <Table />
+        <Table title='Data User' tambahOnClick={tambahOnClick} />
       </main>
     </div>
   );
