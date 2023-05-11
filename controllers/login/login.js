12e4f98fc7878ce42418f5b14d628d2abea9a40f
GilbyKoloay <lolwtf10001@gmail.com>
2023-05-12 07:44:50 +0800

    add APIs to change data

--- controllers/login/login.js
+++ controllers/login/login.js
@@ -10,19 +10,23 @@ export default function login(req, res) {
 
     let isUsernameExist = false;
     let isPasswordTrue = false;
+    let userType = null;
 
     data.forEach(thisUsername => {
       if(username === thisUsername.username) {
         isUsernameExist = true;
         data.forEach((thisPassword, index) => {
-          if(password === thisPassword.password) isPasswordTrue = true;
+          if(password === thisPassword.password) {
+            isPasswordTrue = true;
+            userType = thisPassword.level;
+          };
         });
       }
     });
 
     if(!isUsernameExist) send(res, 404, null, false, 'Username not found.');
     else if(!isPasswordTrue) send(res, 404, null, false, 'Wrong password.');
-    else send(res, 200);
+    else send(res, 200, userType);
   }
   catch(e) {
     send(res, 500, null, false, e.message);
--- router.js
+++ router.js
@@ -6,21 +6,25 @@ import login from './controllers/login/login.js';
 // user
 import userGetAll from './controllers/user/getAll.js';
 import userAdd from './controllers/user/add.js';
+import userChange from './controllers/user/change.js';
 import userDelete from './controllers/user/delete.js';
 
 // laporan
 import laporanGetAll from './controllers/laporan/getAll.js';
 import laporanAdd from './controllers/laporan/add.js';
+import laporanChange from './controllers/user/change.js';
 import laporanDelete from './controllers/laporan/delete.js';
 
 // laporanHarian
 import laporanHarianGetAll from './controllers/laporanHarian/getAll.js';
 import laporanHarianAdd from './controllers/laporanHarian/add.js';
+import laporanHarianChange from './controllers/user/change.js';
 import laporanHarianDelete from './controllers/laporanHarian/delete.js';
 
 // laporanBulanan
 import laporanBulananGetAll from './controllers/laporanBulanan/getAll.js';
 import laporanBulananAdd from './controllers/laporanBulanan/add.js';
+import laporanBulananChange from './controllers/user/change.js';
 import laporanBulananDelete from './controllers/laporanBulanan/delete.js';
 
 
@@ -35,21 +39,25 @@ router.post('/login', login);
 // user
 router.get('/user/getAll', userGetAll);
 router.post('/user/add', userAdd);
+router.patch('/user/change:index', userChange);
 router.delete('/user/delete:index', userDelete);
 
 // laporan
 router.get('/laporan/getAll', laporanGetAll);
 router.post('/laporan/add', laporanAdd);
+router.patch('/laporan/change:index', laporanChange);
 router.delete('/laporan/delete:index', laporanDelete);
 
 // laporanHarian
 router.get('/laporanHarian/getAll', laporanHarianGetAll);
 router.post('/laporanHarian/add', laporanHarianAdd);
+router.patch('/laporanHarian/change:index', laporanHarianChange);
 router.delete('/laporanHarian/delete:index', laporanHarianDelete);
 
 // laporanBulanan
 router.get('/laporanBulanan/getAll', laporanBulananGetAll);
 router.post('/laporanBulanan/add', laporanBulananAdd);
+router.patch('/laporanBulanan/change:index', laporanBulananChange);
 router.delete('/laporanBulanan/delete:index', laporanBulananDelete);
 
 
